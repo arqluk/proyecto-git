5b45d7212329d06b81e7ec1479de2a714286b24f
arqluk <alejandro.lukacher@gmail.com>
2025-07-21 23:22:09 -0300

    Agrega hora al saludo

--- main.js
+++ main.js
@@ -5,10 +5,17 @@
 //     saludo("Rosario", "Buenos días", "10:00 AM");
 
     // Función saludo usando template strings
-function saludo(nombre, mensaje) {
-    console.log(`${mensaje}, ${nombre}!`);
-    }
+// function saludo(nombre, mensaje) {
+//     console.log(`${mensaje}, ${nombre}!`);
+//     }
     
-    // Ejemplos de uso:
-    saludo("Rosario", "¡Hola"); // Resultado: ¡Hola, Rosario!
-    saludo("Pepe", "¡Buen trabajo"); // Resultado: ¡Buen trabajo, Pepe!
+//     // Ejemplos de uso:
+//     saludo("Rosario", "¡Hola"); // Resultado: ¡Hola, Rosario!
+//     saludo("Pepe", "¡Buen trabajo"); // Resultado: ¡Buen trabajo, Pepe!
+
+    // Rama "conflicto"
+function saludo(nombre, mensaje, hora) {
+    console.log(`${mensaje}, ${nombre}. Son las ${hora}.`);
+    }
+
+    saludo("Rosario", "Buenos días", "10:00 AM");
